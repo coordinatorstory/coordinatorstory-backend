79bfb96f517c0238dc1b60b3f3a5bf5a7a5b42fe
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-04-15 23:24:24 -0400

    Init migrations

--- db/migrations/20190415155558_users.js
+++ db/migrations/20190415155558_users.js
@@ -1,8 +1,16 @@
-
-exports.up = function(knex, Promise) {
-  
+exports.up = function(knex) {
+  return knex.schema.createTable('users', users => {
+    users.increments();
+    users
+      .string('username', 255)
+      .notNullable()
+      .unique();
+    users.string('password', 255).notNullable();
+    users.string('title', 255);
+    users.timestamps(true, true);
+  });
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('users');
 };
--- db/migrations/20190415155624_stories.js
+++ db/migrations/20190415155624_stories.js
@@ -1,8 +1,13 @@
-
-exports.up = function(knex, Promise) {
-  
+exports.up = function(knex) {
+  return knex.schema.createTable('stories', stories => {
+    stories.increments();
+    stories.string('title', 255).notNullable();
+    stories.string('country', 255).notNullable();
+    stories.text('description', 255).notNullable();
+    stories.timestamps(true, true);
+  });
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('stories');
 };
