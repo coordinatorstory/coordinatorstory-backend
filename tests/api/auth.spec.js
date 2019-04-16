540e5c20505449b225b8a2b32ccd3e873f4c5fe7
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-04-16 13:17:26 -0400

    Add auth routes tests

---
+++ tests/api/auth.spec.js
@@ -0,0 +1,66 @@
+const request = require('supertest');
+const server = require('../../api/server');
+const db = require('../../data/db');
+
+describe('Auth routes', () => {
+  const mockUser = {
+    username: 'JaneUser',
+    password: 'password',
+    first_name: 'Jane',
+    last_name: 'Jacobs',
+    email: 'jjacobs@mail.com',
+    title: 'Head Coordinator'
+  };
+
+  afterAll(async () => {
+    const registeredUser = await db.users.getBy({ username: mockUser.username });
+    await db.users.delete(registeredUser.id);
+  });
+
+  describe('Register', () => {
+    it('should register a new user', async () => {
+      let res = await request(server)
+        .post('/api/auth/register')
+        .send(mockUser);
+      expect(res.status).toBe(201);
+    });
+
+    it('should respond with 400 when not required fields are absent', async () => {
+      let res = await request(server)
+        .post('/api/auth/register')
+        .send({
+          username: 'InvalidUser'
+        });
+      expect(res.status).toBe(400);
+    });
+  });
+
+  describe('Login', () => {
+    it('should allow a user to log in', async () => {
+      let res = await request(server)
+        .post('/api/auth/login')
+        .send({
+          username: mockUser.username,
+          password: mockUser.password
+        });
+      expect(res.status).toBe(200);
+    });
+
+    it('should respond with 401 when credentials are invalid', async () => {
+      let res = await request(server)
+        .post('/api/auth/login')
+        .send({
+          username: mockUser.username,
+          password: 'bad-password'
+        });
+      expect(res.status).toBe(401);
+      res = await request(server)
+        .post('/api/auth/login')
+        .send({
+          username: 'nonexistentUser',
+          password: mockUser.password
+        });
+      expect(res.status).toBe(401);
+    });
+  });
+});
