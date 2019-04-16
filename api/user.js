cbe0025eda2fcb52f94b9e1493f3c2fedb957fe6
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-04-16 18:54:45 -0400

    Add POST route

--- api/user.js
+++ api/user.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const db = require('../data/db');
+const { validateStory } = require('./validators');
 
 const usersRouter = express.Router();
 
 usersRouter.get('/stories', getUserStories);
+usersRouter.post('/stories', createUserStory);
 
 async function getUserStories(req, res) {
   try {
@@ -11,6 +13,24 @@ async function getUserStories(req, res) {
     res.status(200).json(stories);
   } catch (error) {
     res.status(500).json({ error: 'Cannot get stories.' });
+  }
+}
+
+async function createUserStory(req, res) {
+  try {
+    const story = req.body;
+    const { error } = validateStory(story);
+    if (error) {
+      res.status(400).json({
+        error: error.details[0].message
+      });
+    } else {
+      const newStory = await db.stories.create(story);
+      res.status(201).json(newStory);
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Cannot create story.' });
   }
 }
 
