3cf012f00522b78300316d346ed0ff61f9b5ad67
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-04-16 14:09:20 -0400

    Fix tests

--- tests/api/stories.spec.js
+++ tests/api/stories.spec.js
@@ -3,7 +3,7 @@ const server = require('../../api/server');
 const db = require('../../data/db');
 
 describe('Story routes', () => {
-  describe('GET /stories', () => {
+  describe('GET /api/stories', () => {
     it('should respond with 200', async () => {
       let res = await request(server).get('/api/stories');
       expect(res.status).toBe(200);
@@ -12,7 +12,7 @@ describe('Story routes', () => {
     it('should return all stories when not logged in', async () => {
       const stories = await db.stories.getAll();
       let res = await request(server).get('/api/stories');
-      expect(res.status.body).toHaveLength(stories.length);
+      expect(res.body).toHaveLength(stories.length);
     });
   });
 });
