5df7bc47a2e28df17970316647e277003a1254cb
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-04-16 14:01:10 -0400

    Add stories tests

---
+++ tests/api/stories.spec.js
@@ -0,0 +1,18 @@
+const request = require('supertest');
+const server = require('../../api/server');
+const db = require('../../data/db');
+
+describe('Story routes', () => {
+  describe('GET /stories', () => {
+    it('should respond with 200', async () => {
+      let res = await request(server).get('/api/stories');
+      expect(res.status).toBe(200);
+    });
+
+    it('should return all stories when not logged in', async () => {
+      const stories = await db.stories.getAll();
+      let res = await request(server).get('/api/stories');
+      expect(res.status.body).toHaveLength(stories.length);
+    });
+  });
+});
