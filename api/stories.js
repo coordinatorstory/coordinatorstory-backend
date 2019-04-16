02c47a0d88eede705f358a33da7b4d2b8b31fd73
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-04-16 14:09:02 -0400

    Add first stories route

--- api/stories.js
+++ api/stories.js
@@ -3,8 +3,27 @@ const db = require('../data/db');
 
 const storiesRouter = express.Router();
 
-storiesRouter.post('/stories', getStories);
+// GET /stories -> all stories for user logged in, all for not logged in, by country with query string
+// POST /stories -> create story
 
-async function getStories(req, res) {}
+// GET /stories/<id> -> single story
+// PUT /stories/<id> -> update story, protected
+// DELETE /stories/<id> -> delete story, protected
+
+storiesRouter.get('/', getStories);
+
+async function getStories(req, res) {
+  try {
+    if (!req.user) {
+      const stories = await db.stories.getAll();
+      res.status(200).json(stories);
+    } else {
+      const stories = await db.stories.getAllBy({ user_id: req.user.userId });
+      res.status(200).json(stories);
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Cannot get stories.' });
+  }
+}
 
 module.exports = storiesRouter;
