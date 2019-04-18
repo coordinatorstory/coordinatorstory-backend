415a87b2112c5ef80a755f3b458948279e1f58a8
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-04-18 17:31:38 -0400

    Tidy up

--- data/schemas.js
+++ data/schemas.js
@@ -1,36 +1,32 @@
 const Joi = require('joi');
 
-const userSchema = {
-  username: Joi.string()
-    .label('Username')
-    .alphanum()
-    .min(3)
-    .max(255)
-    .required(),
-  password: Joi.string()
-    .label('Password')
-    .min(6)
-    .required(),
-  email: Joi.string()
-    .label('Email address')
-    .email({ minDomainAtoms: 2 })
-    .required(),
-  first_name: Joi.string()
-    .label('First name')
-    .required(),
-  last_name: Joi.string()
-    .label('Last name')
-    .required(),
-  title: Joi.string().label('Title')
-};
-
-const storySchema = {
-  title: Joi.string().required(),
-  country: Joi.string().required(),
-  description: Joi.string().required()
-};
-
 module.exports = {
-  userSchema,
-  storySchema
+  userSchema: {
+    username: Joi.string()
+      .label('Username')
+      .alphanum()
+      .min(3)
+      .max(255)
+      .required(),
+    password: Joi.string()
+      .label('Password')
+      .min(6)
+      .required(),
+    email: Joi.string()
+      .label('Email address')
+      .email({ minDomainAtoms: 2 })
+      .required(),
+    first_name: Joi.string()
+      .label('First name')
+      .required(),
+    last_name: Joi.string()
+      .label('Last name')
+      .required(),
+    title: Joi.string().label('Title')
+  },
+  storySchema: {
+    title: Joi.string().required(),
+    country: Joi.string().required(),
+    description: Joi.string().required()
+  }
 };
