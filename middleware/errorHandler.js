345782a98f203fb5b18bba80a254cee50822fe39
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-04-18 14:30:44 -0400

    Log errors with status code above 499

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,14 +1,14 @@
 module.exports = (err, req, res, next) => {
   const env = process.env.DB_ENV;
+  let error = err.message;
+  const statusCode = err.statusCode || 500;
 
-  if (env !== 'testing') {
-    console.error(`Error: ${req.method} ${req.path}`);
+  if (env !== 'testing' && err.statusCode > 499) {
+    console.error(`Error during ${req.method} at ${req.path}`);
     console.error(err.stack);
   }
 
-  let error = err.message;
-
   if (env === 'production') error = 'An error occurred while processing your request.';
 
-  res.status(err.statusCode || 500).json({ error });
+  res.status(statusCode).json({ error });
 };
