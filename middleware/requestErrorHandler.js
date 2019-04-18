429e11da41aa94e7232f7a72227085c1fb484b62
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-04-18 13:52:07 -0400

    Send friendly error message in production

--- middleware/requestErrorHandler.js
+++ middleware/requestErrorHandler.js
@@ -1,6 +1,14 @@
 module.exports = (err, req, res, next) => {
-  if (process.env.DB_ENV !== 'testing') {
+  const env = process.env.DB_ENV;
+
+  if (env !== 'testing') {
+    console.error(`Error: ${req.method} ${req.path}`);
     console.error(err.stack);
   }
-  res.status(err.statusCode || 500).json({ error: err.message });
+
+  let error = err.message;
+
+  if (env === 'production') error = 'An error occurred while processing your request.';
+
+  res.status(err.statusCode || 500).json({ error });
 };
