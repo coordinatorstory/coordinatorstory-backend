4bb9cb434ffb839b039576a68d69dddebe660ece
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-04-18 15:49:43 -0400

    Mock additional error types

--- common/errors.js
+++ common/errors.js
@@ -6,6 +6,22 @@ class RequestError extends Error {
   }
 }
 
+// class BadRequestError extends Error {
+//   constructor(message) {
+//     super(message);
+//     this.name = 'BadRequestError';
+//     this.statusCode = 400;
+//   }
+// }
+
+// class NotFoundError extends Error {
+//   constructor(message) {
+//     super(message);
+//     this.name = 'NotFoundError';
+//     this.statusCode = 404;
+//   }
+// }
+
 class DatabaseError extends Error {
   constructor(message) {
     super(message);
